e4d698ab2223173a663d450b996c3ae50e67b02e
mydigitalcrown <sanjana@trinketsinstitute.com>
2025-12-31 16:36:55 +0530

    fix: Force favicon to use favicon-32x32.png and remove vercel.svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,6 +115,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`}>
       <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link 
           rel="alternate" 
           type="application/rss+xml" 
